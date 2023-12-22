909ed00b8fcd22dd8255d92b3515abb1f644e50d
Richard Freedman <rfreedma@haverford.edu>
2023-12-22 11:07:16 -0500

    Update sound_map_streamlit.py

--- 06_SoundMap/sound_map_streamlit.py
+++ 06_SoundMap/sound_map_streamlit.py
@@ -19,14 +19,33 @@ st.header('Bi-Co Sound Survey Computational Essay')
 st.markdown('By Logan Griffin, Luke Sheppard, Reed Solomon, and Jade Yu')
 st.markdown('Sounds of Silence: A Sound Survey of the Bi-Co During Finals Week')
 
-st.dataframe(full_data)
+
+
+st.sidebar.write("Filter Data")
+
+    # Get the min and max values for the volume column
+min_val = full_data['volume'].min()
+max_val = full_data['volume'].max()
+
+# Create a slider for the volume column
+volume_range = st.sidebar.slider('Volume Range', min_val, max_val, (min_val, max_val))
+
+# Filter the dataframe based on the selected volume range
+if 'filtered_df' not in st.session_state:
+    st.session_state.filtered_df = full_data[(full_data['volume'] >= volume_range[0]) & (full_data['volume'] <= volume_range[1])]
+else: 
+    st.session_state.filtered_df = full_data[(full_data['volume'] >= volume_range[0]) & (full_data['volume'] <= volume_range[1])]
+
+show_data = st.sidebar.checkbox('Show Filtered Data', value=False)
+if show_data:
+    st.dataframe(st.session_state.filtered_df)
 
 # here is the sidebar button to show the map
 show_map = st.sidebar.checkbox('Show Map', value=False)
 px.set_mapbox_access_token("pk.eyJ1IjoiZnJlZWRtYW4iLCJhIjoiY2xxZnJndjRmMTBwOTJtcXc1YjFlNjcxdCJ9.ih5e7JGYt6Izae0V5gzyEw")
 
 if show_map:
-    fig = px.scatter_mapbox(full_data, 
+    fig = px.scatter_mapbox(st.session_state.filtered_df, 
                         lat='longitude', 
                         lon='latitude', 
                         hover_name='location', 
